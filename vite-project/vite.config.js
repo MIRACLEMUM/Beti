c19e22d1a0cf2682390c915d376f7512bc270d02
Miracle sunady <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-07-01 08:18:43 +0100

    HHH

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  base: '/Beti/', // 👈 this must match your repo name exactly
+  plugins: [tailwindcss(), react()],
 })
